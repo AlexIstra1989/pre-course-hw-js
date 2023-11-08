56db3e5fa736f2abaf19d075f9a8a3aa80ebed5d
AlexIstra1989 <alex888fcsm@gmail.com>
2023-11-08 18:12:54 +0300

    add 04.js

--- lesson-01/03.js
+++
@@ -1,3 +0,0 @@
-// code
-let number1 = Math.floor(Math.random() * 100);
-console.log(number1);
---
+++ lesson-01/04.js
@@ -0,0 +1,12 @@
+// code
+let number = 1;
+
+let result1 = number++;  
+let result2 = ++number;  
+let result3 = number--;  
+let result4 = --number;  
+
+console.log(result1); 
+console.log(result2); 
+console.log(result3); 
+console.log(result4); 
