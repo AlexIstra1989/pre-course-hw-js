6f273d392621fe7a6980db35758947790c667aa9
AlexIstra1989 <alex888fcsm@gmail.com>
2024-01-31 01:28:40 +0300

    add les 2 06.js

---
+++ lesson-02/06.js
@@ -0,0 +1,22 @@
+let passportWithAddress = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "LA"
+    }
+};
+
+deepclone = obj => {
+    let cloned = {};
+    for (i in obj) cloned [i] = typeof obj[i] == 'object' ? deepclone(obj[i]) : obj[i];
+    return cloned;
+}
+
+let passportWithAddress2 = deepclone(passportWithAddress);
+
+passportWithAddress2.address.city = "Bobryisk"
+
+
+console.log(passportWithAddress);
+console.log(passportWithAddress2);
